e0a71f98c2e7d7d0836394037dc9769e9df10076
aimanasifkhan3 <aimanasifkhan3@gmail.com>
2022-06-30 18:03:03 +0530

    initial commit

--- models/users.js
+++
@@ -1,9 +0,0 @@
-var mongoose              = require("mongoose");
-    passportLocalMongoose = require("passport-local-mongoose");
-               UserSchema = new mongoose.Schema({
-	username: String,
-	password: String,
-});
-UserSchema.plugin(passportLocalMongoose);
-var user = mongoose.model("User",UserSchema);
-module.exports = user;
